25a969b21b34b24850ddedc369dd33ad04baea64
Jørgen Rieck <jurgemaister@gmail.com>
2013-05-24 08:17:13 +0200

    url improved

--- lib/cinch/plugins/urlmagic.rb
+++ lib/cinch/plugins/urlmagic.rb
@@ -12,18 +12,20 @@ module Cinch
 			def execute(m, url)
 				if url.match(/open\.spotify/)
 					return
-				elsif url.length < 80
-					return
 				end
 
 				agent = Mechanize.new
 				page = agent.get(url)
 				title = page.title.gsub(/[\r\n\t]/, '')
-				Bitly.use_api_version_3
-				bitly = Bitly.new('o_7ao1emfe9u', 'R_b29e38be56eb1f04b9d8d491a4f5b344')
-				shortURL = bitly.shorten(url)
+				shortURL = ""
+
+				if url.length > 80
+					Bitly.use_api_version_3
+					bitly = Bitly.new('o_7ao1emfe9u', 'R_b29e38be56eb1f04b9d8d491a4f5b344')
+					shortURL = "(" + bitly.shorten(url) + ")"
+				end
 				
-				m.reply %-"#{title}" (#{shortURL.short_url})-
+				m.reply %-"#{title}" #{shortURL.short_url}-
 			end
 		end
 	end
