3cf03c3f41076ce95c2aeed76aafda31fd7f0349
Jørgen Rieck <jurgemaister@gmail.com>
2013-05-24 17:00:37 +0200

    Space

--- lib/cinch/plugins/ask.rb
+++ lib/cinch/plugins/ask.rb
@@ -12,7 +12,7 @@ module Cinch
 
 		  def execute(m, nick)
 		  	unless nick.nil?
-		  		nick = %-Hør nå her #{nick}:-
+		  		nick = %-Hør nå her #{nick}: -
 		  	end
 		  	m.reply %-#{nick}Du trenger ikke å be om å spørre, eller å be om en "ekspert" på noe. Bare skriv ditt spørsmål i kanalen. Hvis noen kan hjelpe, vil de gjøre det. Du kan ikke forvene å få et svar umiddelbart. Vær tålmodig. Vær villig til å vente en liten stund etter å ha spurt i tilfelle noen ser spørsmålet ditt senere og prøver å hjelpe. Hvis du forlater kanalen for raskt kan du få glipp av nyttige svar.-
 		  end
