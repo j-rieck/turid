fc422ff20778fdf6b3c1b7164e47be0980fbd93d
Jørgen Rieck <jurgemaister@gmail.com>
2013-05-24 08:09:32 +0200

    Added xkcd

---
+++ lib/cinch/plugins/xkcd.rb
@@ -0,0 +1,29 @@
+require 'cinch'
+require 'mechanize'
+
+module Cinch
+	module Plugins
+		class Xkcd
+			include Cinch::Plugin
+
+			match /xkcd\s?(\d+)?/
+
+			def execute(m, id)
+				unless id.nil?
+					strip = id
+				end
+
+				uri =  "http://xkcd.com/" + strip
+		    agent = Mechanize.new
+		    begin
+		      page = agent.get(uri)
+		    rescue Mechanize::ResponseCodeError
+		      m.reply "Sorrymac. Stripa finnes ikke"
+		      return
+		    end
+		    title = page.title.gsub(/[\r\n]/, '')
+		    m.reply %-"#{title}" #{uri}"-
+			end
+		end
+	end
+end
