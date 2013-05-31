278e047120ef06ddf776d6f4f64b35cd4fdd776c
Jørgen Rieck <jurgemaister@gmail.com>
2013-05-31 14:39:24 +0200

    First working yr.no

--- lib/cinch/plugins/yr.rb
+++ lib/cinch/plugins/yr.rb
@@ -1,5 +1,6 @@
 # encoding=utf-8
 require 'nokogiri'
+require 'open-uri'
 
 module Cinch
 	module Plugins
@@ -13,36 +14,45 @@ module Cinch
 				File.readlines(Dir.pwd + '/lib/data/noreg.csv', :encoding =>'UTF-8').map do |line|
 				  @@places.push(line.split(/\t/))
 				end
+				@@places.sort_by! {|x,y,z|z}
 			end
 
 			def find (loc, reg1 = nil, reg2 = nil)
-				debug "finding location #{loc}"
+				# Filtering out unmatched fylke
+				unless reg2.nil?
+					@@places.delete_if {|knr, name, pri, tnn, tnb, ten, k, f| not f.match(/\b#{reg2}\b/i)}
+				end
+
+				# Filtering out unmatched kommune
+				unless reg1.nil?
+					@@places.delete_if {|knr, name, pri, tnn, tnb, ten, k, f| not k.match(/\b#{reg1}\b/i) and not f.match(/\b#{reg1}\b/i)}
+				end
+
+				# Filtering out unmatched place
+				@@places.delete_if {|knr, name| not name.match(/\b#{loc}\b/i)}
+
 				@@places.each do |place|
-					# reg2 filter
-					unless reg2.nil?
-						unless place[7].downcase.match(/\b#{reg2.downcase}\b/)
-							debug "reg2 present and not match fylke"
-							next
-						end
-					end
-
-					# reg1 filter
-					unless reg1.nil?
-						unless place[6].downcase.match(/\b#{reg1.downcase}\b/)
-							debug "reg1 present and not match kommune"
-							unless place[7].downcase.match(/\b#{reg1.downcase}\b/)
-								debug "reg1 present and not match fylke"
-								next
-							end
-						end
-					end
-
-					if place[1].downcase.match(/\b#{loc.downcase}\b/u)
-						return place[13]
+					if place[1].match(/\b#{loc}\b/i)
+						return place[12]
 					end
 				end
 
 				return "Fant desverre ikke stedet du søkte etter. Prøv et annet"
+			end
+
+			def forecast (uri)
+				begin
+			      doc = Nokogiri::XML(open(uri))
+			      name = doc.css('location name').text
+			      temp = doc.css('observations weatherstation:first temperature').attr('value')
+			      windDir = doc.css('observations weatherstation:first windDirection').attr('name')
+			      windSpd = doc.css('observations weatherstation:first windSpeed').attr('mps')
+
+			      return "#{name}: For øyeblikket #{temp}°C; Vind #{windSpd} m/s #{windDir}."
+			    rescue OpenURI::HTTPError
+			      m.reply "Får ikke kontakt med yr.no :/"
+			      return
+			    end
 			end
 
 			def execute(m, loc, reg1, reg2)
@@ -53,7 +63,14 @@ module Cinch
 					return
 				end
 
-				m.reply find loc, reg1, reg2
+				uri = find loc, reg1, reg2
+
+				unless uri.nil?
+					m.reply forecast uri
+					return
+				end
+
+				m.reply "Fant desverre ikke stedet du søkte etter. Prøv et annet"
 			end
 		end
 	end
