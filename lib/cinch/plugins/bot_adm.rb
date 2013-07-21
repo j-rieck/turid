80d34b6ac588deb5125e4fab6f4c30d6254eaae9
Tri Nguyen <mail@trimn.net>
2013-07-21 03:25:44 +0200

    Add join/part feature.

--- lib/cinch/plugins/bot_adm.rb
+++ lib/cinch/plugins/bot_adm.rb
@@ -9,10 +9,22 @@ module Cinch
       include Cinch::Plugin
 
       match /faggot( .+)?/, method: :faggot
-
       def faggot(m, me)
+        m.reply "thoijasdf"
         return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
         m.reply %-jmiaster er sexy \- #{me}-
+      end
+
+      match /join\s+(#[#\w\d_-]+)/, method: :join
+      def join(m, channel)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        bot.join channel
+      end
+
+      match /part\s+(#[#\w\d_-]+)/, method: :part
+      def part(m, channel)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        bot.part channel
       end
 
     end
