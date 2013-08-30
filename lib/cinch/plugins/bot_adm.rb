7dc803827f013916c6a4252032bebdf753a6c342
Jørgen Rieck <jurgemaister@gmail.com>
2013-08-30 17:12:40 +0200

    Added admin functionality

--- lib/cinch/plugins/bot_adm.rb
+++ lib/cinch/plugins/bot_adm.rb
@@ -9,10 +9,35 @@ module Cinch
       include Cinch::Plugin
 
       match /faggot( .+)?/, method: :faggot
+      match /say (\S+) (.*)/, method: :say
+      match /op (\S+) (\S+)/, method: :op
+      match /deop (\S+) (\S+)/, method: :deop
+      match /kick (\S+) (.*) (\S+)/, method: :kick
 
       def faggot(m, me)
         return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
         m.reply %-jmiaster er sexy \- #{me}-
+      end
+
+      def say(m, channel, message)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        Channel("##{channel}").privmsg(message)
+      end
+
+      def op(m, channel, user)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        Channel("##{channel}").op(user)
+      end
+
+      def deop(m, channel, user)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        Channel("##{channel}").deop(user)
+      end
+
+      def kick(m, channel, user, reason)
+        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        reason = reason || "Fuck off!"
+        Channel("##{channel}").kick(user)
       end
 
     end
