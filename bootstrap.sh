5e471f83ab7f61faf8624186060704a43b3c3034
Tri M. Nguyen <mail@trimn.net>
2013-12-04 20:43:21 +0100

    Update Vagrantfile and bootstrap.sh to setup the fagbot environment.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,9 @@ sudo apt-get install -y curl
 # apt-get upgrade
 
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+sudo apt-get install libcurl3-dev
+
+## 
+cd /vagrant/
+bundle install --path ~/.gem
