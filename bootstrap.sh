5c99caa95406a96f1b549f6bcee082a4ede8324a
Tri M. Nguyen <mail@trimn.net>
2013-12-05 01:52:35 +0100

    Update bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,27 @@ sudo apt-get install -y curl
 
 sudo apt-get install -y libcurl3-dev
 
+mkdir -p ~/Downloads/
+cd ~/Downloads/
+
+## Install Java
+wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-i586.tar.gz"
+tar xvzf jdk-7u45-linux-i586.tar.gz
+sudo mkdir -p /usr/lib/jvm/
+sudo mv jdk1.7.0_45 /usr/lib/jvm
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0_45/jre/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_45/bin/javac" 1
+
+## Install elastic search
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
+sudo dpkg -i elasticsearch-0.90.7.deb
+sudo service elasticsearch start
+
+## Cleanup
+rm jdk-7u45-linux-i586.tar.gz
+rm elasticsearch-0.90.7.deb
+
+
 ## 
 cd /vagrant/
 bundle install --path ~/.gem
