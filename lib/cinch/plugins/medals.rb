5998f6d92b7ae1178742545aea3a3001b82e1948
Jørgen Rieck <jurgemaister@gmail.com>
2014-02-09 12:51:13 +0100

    Added country input for medals

--- lib/cinch/plugins/medals.rb
+++ lib/cinch/plugins/medals.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'cinch'
 require 'open-uri'
 
@@ -6,16 +8,21 @@ module Cinch
 		class Medals
       include Cinch::Plugin
 
-      match /ol/
-      def execute(m)
+      match /ol\s?(.+)?/
+      def execute(m, c)
         url = "http://olympics.clearlytech.com/api/v1/medals"
         data = JSON.parse(open(url).read)
-        norway = data.select {|d| d["country_name"] == "Norway"}[0]
-        gold = norway["gold_count"]
-        silver = norway["silver_count"]
-        bronze = norway["bronze_count"]
 
-        m.reply "Medaljer i Sotsji: #{gold} gull, #{silver} sølv og #{bronze} bronse"
+        if c.nil?
+          c = "Norway"
+        end
+
+        country = data.select {|d| d["country_name"] == c}[0]
+        gold = country["gold_count"]
+        silver = country["silver_count"]
+        bronze = country["bronze_count"]
+
+        m.reply "Medaljer i Sotsji (#{c}): #{gold} gull, #{silver} sølv og #{bronze} bronse"
       end
     end
   end
