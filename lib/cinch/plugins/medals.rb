cb7a15327219446c9cfd8fd79740ebe307518cb6
Jørgen Rieck <jurgemaister@gmail.com>
2014-02-18 13:33:35 +0100

    Changed data source for medals to tmn.io

--- lib/cinch/plugins/medals.rb
+++ lib/cinch/plugins/medals.rb
@@ -4,21 +4,21 @@ require 'cinch'
 require 'open-uri'
 
 module Cinch
-	module Plugins
-		class Medals
+  module Plugins
+    class Medals
       include Cinch::Plugin
 
       match /ol\s?(.+)?/
       def execute(m, c)
-        url = "http://mapi.sochi2014.com/v1/en/olympic/medal/rating"
-        data = JSON.parse(open(url).read)
-
         if c.nil?
           c = "Norway"
         end
-        # debug data.to_s 
-        country = data.select {|d| d["name"].match(/#{c}/i)}[0]#debug d.to_s}# )[0]} #d["name"] == c
-        m.reply "Medaljer i Sotsji (#{country["name"]}): #{country["gold"]} gull, #{country["silver"]} sølv og #{country["bronze"]} bronse"
+
+        url = "http://olympics.api.tmn.io/medals/" + c
+        data = JSON.parse(open(url).read)[0]
+
+        # debug data.to_s
+        m.reply "Medaljer i Sotsji (#{data["c"]}): #{data["g"]} gull, #{data["s"]} sølv og #{data["b"]} bronse"
       end
     end
   end
