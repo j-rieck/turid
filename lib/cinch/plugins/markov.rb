84becb0cec0203b3bdae3f06080101bb67e0c65a
Håkon Lerring <hakon@powow.no>
2014-07-14 23:56:53 +0200

    Add markov chain plugin

---
+++ lib/cinch/plugins/markov.rb
@@ -0,0 +1,31 @@
+require 'marky_markov'
+require 'cinch'
+
+module Cinch
+	module Plugins
+		class Markov
+		  include Cinch::Plugin
+
+		  listen_to :channel, method: :execute
+		  timer 60, method: :save_dict
+
+		  def initialize(*args)
+		  	super(*args)
+		  	@markov = MarkyMarkov::Dictionary.new('markov_dictionary')
+		  end
+
+		  def execute(m)
+		  	if(Regexp.new("^" + Regexp.escape(m.bot.nick + ":" )) =~ m.message)
+		  		m.reply @markov.generate_n_sentences(1)
+		  	else
+		  		@markov.parse_string(m.message)
+		  	end
+		  end
+
+		  def save_dict
+		  	@markov.save_dictionary!
+		  end
+
+		end
+	end
+end
