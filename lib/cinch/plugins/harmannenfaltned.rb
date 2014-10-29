acd92de1addc5dcf16ce100a9bf71a243e1e94de
Jørgen Rieck <jurgemaister@gmail.com>
2014-10-29 14:57:42 +0100

    harmannenfaltnedendada?

---
+++ lib/cinch/plugins/harmannenfaltned.rb
@@ -0,0 +1,22 @@
+require 'cinch'
+
+module Cinch
+	module Plugins
+		class Harmannenfaltned
+		  include Cinch::Plugin
+
+		  match /^har mannen falt ned\??/i, use_prefix: false
+
+		  def execute(m)
+		  	url = "http://www.vondess.com/mannen/api"
+		  	data = JSON.parse(open(url).read)
+		  	debug data.map{|k,v| "#{k}=#{v}"}.join('&')
+		  	if data["falt_ned"] == false
+		  		m.reply "Nei, Mannen har ikke falt ned enda"
+		  	else
+		  		m.reply "Jepp! Sjekk VG!"
+		  	end
+		  end
+		end
+	end
+end
