2335c39175f6b6b1bbd807eab2e03e8bc4c874fa
Jørgen Rieck <jurgemaister@gmail.com>
2014-11-25 15:33:28 +0100

    Little dickup in the code

--- lib/cinch/plugins/dick.rb
+++ lib/cinch/plugins/dick.rb
@@ -10,7 +10,7 @@ module Cinch
 			def execute(m, n)
 				reply = ""
 				if n.to_i > 5
-					reply "Nå må du ikke være grådig. Maks 5 peniser."
+					reply = "Nå må du ikke være grådig. Maks 5 peniser."
 				else
 					dicks = Dicks::API.get_dicks(n)
 					reply = dicks.join(" ")
