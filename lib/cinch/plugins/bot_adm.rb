897c16954603870ed9d3823af5002611f86e95a4
Jørgen Rieck <jurgemaister@gmail.com>
2014-12-04 15:07:14 +0100

    A bit nicer filter for bot_adm

--- lib/cinch/plugins/bot_adm.rb
+++ lib/cinch/plugins/bot_adm.rb
@@ -20,50 +20,56 @@ module Cinch
       match /conf reload/, method: :conf_reload
       match /nick (\S+)/, method: :nick
 
+      def authorized?(m)
+        unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+          m.reply "Unauthorized access. This incident will be reported"
+          throw "RoflException"
+        end
+      end
+
       def faggot(m, me)
-        m.reply "thoijasdf"
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         m.reply %-jmiaster er sexy \- #{me}-
       end
 
       def join(m, channel)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         bot.join channel
       end
 
       def part(m, channel)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         bot.part channel
       end
 
       def say(m, channel, message)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         Channel("##{channel}").privmsg(message)
       end
 
       def op(m, channel, user)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         Channel("##{channel}").op(user)
       end
 
       def deop(m, channel, user)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         Channel("##{channel}").deop(user)
       end
 
       def kick(m, channel, user, reason)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         reason ||= "Fuck off!"
         Channel("##{channel}").kick(user, reason)
       end
 
       def conf_reload (m)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         $conf = Settings.new("config.json")
       end
 
       def nick(m, nick)
-        return unless $conf.admins.include?({"nick"=>"#{m.user}", "host"=>"#{m.user.host}"})
+        authorized? m
         bot.nick = nick
         $conf.nick = nick
       end
@@ -73,7 +79,6 @@ module Cinch
           bot.nick = $conf.nick
         end
       end
-
     end
   end
 end
