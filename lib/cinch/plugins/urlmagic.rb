fa07be603e6ccfb352ced577d4169f378b746f5d
Tri M. Nguyen <mail@trimn.net>
2015-02-06 18:39:24 +0100

    Update urlmagic - old code is back...

--- lib/cinch/plugins/urlmagic.rb
+++ lib/cinch/plugins/urlmagic.rb
@@ -12,32 +12,32 @@ module Cinch
 			match /(https?:\/\/[^\s]+)/, use_prefix: false
 
 			def execute(m, url)
-				if url.match(/open\.spotify/)
-					return
-				end
-
-				title = nil
-				shortURL = nil
-
-				open(url).find {|line| 
-					title = line.match(/<title>(.*)<\/title>/)
-				}
-
-				unless title.nil?
-					title = title[1].gsub(/[\r\n\t]/, '')
+				agent = Mechanize.new
+				page = agent.get(url)
+				begin
+					title = page.title.gsub(/[\r\n\t]/, '')
 					title = title.strip
 					title = title.gsub(/\s{2,}/, ' ')
+				rescue
+					title nil
+					debug "could not get title"
+				end
+				shortURL = ""
 
-					if url.length > 80
-						Bitly.use_api_version_3
-						bitly = Bitly.new('o_7ao1emfe9u', 'R_b29e38be56eb1f04b9d8d491a4f5b344')
-						shortURL = bitly.shorten(url)
-						shortURL = "(" + shortURL.short_url + ")"
-					end
-
-					m.reply %-"#{title}" #{shortURL}-
+				if url.length > 80
+					Bitly.use_api_version_3
+					bitly = Bitly.new('o_7ao1emfe9u', 'R_b29e38be56eb1f04b9d8d491a4f5b344')
+					shortURL = bitly.shorten(url)
+					shortURL = "(" + shortURL.short_url + ")"
 				end
 
+				page = nil
+				agent = nil
+				GC.start
+
+				unless title.nil?
+					m.reply %-"#{title}" #{shortURL}-
+				end
 			end
 		end
 	end
