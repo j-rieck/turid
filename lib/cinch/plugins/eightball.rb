fa2a78201c324b7af8a4d91d6cc737589e537f82
Jørgen <jurgemaister@gmail.com>
2016-10-27 14:55:34 +0200

    Fixed some shit

--- lib/cinch/plugins/eightball.rb
+++ lib/cinch/plugins/eightball.rb
@@ -7,23 +7,22 @@ module Cinch
 
 		  set plugin_name: "Eightball"
 
-		  match /^!8ball |\?$/i, use_prefix: false
-
-			answers = [
-					"No!",
-					"Yes!",
-					"Of course.",
-					"All fingers point to yes.",
-					"All fingers point to no.",
-					"It's likely.",
-					"Ask again later.",
-					"You may rely on it.",
-					"Cannot predict now.",
-					"Don't count on it.",
-					"Outlook not so good.",
-					"Without a doubt."
-			]
-
+		  answers = [
+			"No!",
+			"Yes!",
+			"Of course.",
+			"All fingers point to yes.",
+			"All fingers point to no.",
+			"It's likely.",
+			"Ask again later.",
+			"You may rely on it.",
+			"Cannot predict now.",
+			"Don't count on it.",
+			"Outlook not so good.",
+			"Without a doubt."
+		  ]
+			
+		  match /^8ball/i, use_prefix: false
 		  def execute(m)
 		  	sleep(1)
 		  	m.reply "#{m.user.nick}: #{answers.sample}"
